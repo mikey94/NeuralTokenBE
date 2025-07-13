07071eb86f54f8e303ea6b53dd4f9e90e6b94c5b
Buwaneka Ranatunga <bsirinath@gmail.com>
2025-07-13 10:22:40 +0530

    New data added for cryptocurrencies and logic update

---
+++ attention.py
@@ -0,0 +1,19 @@
+from tensorflow.keras.layers import Layer
+from tensorflow.keras.utils import register_keras_serializable
+import tensorflow as tf
+
+@register_keras_serializable()
+class Attention(Layer):
+    def __init__(self, **kwargs):
+        super(Attention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.W = self.add_weight(name="att_weight", shape=(input_shape[-1], 1), initializer="normal")
+        self.b = self.add_weight(name="att_bias", shape=(input_shape[1], 1), initializer="zeros")
+        super(Attention, self).build(input_shape)
+
+    def call(self, x):
+        e = tf.keras.backend.tanh(tf.keras.backend.dot(x, self.W) + self.b)
+        a = tf.keras.backend.softmax(e, axis=1)
+        output = x * a
+        return tf.keras.backend.sum(output, axis=1)
